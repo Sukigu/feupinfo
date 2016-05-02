a0fa90a3be532deab0651da356f1bf7e32ed1b71
Gustavo Silva <gustavo.silva@fe.up.pt>
2016-05-03 00:46:58 +0100

    Logging POSTs

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 var express = require('express');
-var app = express();
+	app = express(),
+	bodyParser = require('body-parser');
 
 var port = process.env.PORT || 8080;
 
+app.use(bodyParser.json());
+
 app.get('/', function (req, res) {
-  if (req.query['hub.verify_token'] === 't0Iv74RLdyS0pHpGDaI27Kf4Dv213w8d') {
-    res.send(req.query['hub.challenge']);
-  } else {
-    res.send('Error, wrong validation token');    
-  }
+	if (req.query['hub.verify_token'] === 't0Iv74RLdyS0pHpGDaI27Kf4Dv213w8d') {
+		res.send(req.query['hub.challenge']);
+	} else {
+		res.send('Error, wrong validation token');    
+	}
+});
+
+app.post('/', function(req, res) {
+	console.log(req.body);
 });
 
 app.listen(port, function () {
-  console.log('Now listening on port ' + port + '...');
+	console.log('Now listening on port ' + port + '...');
 });
