5cc174d32901d775c9d9f03e2cd610285518663f
Gustavo Silva <gustavo.silva@fe.up.pt>
2016-05-06 03:42:38 +0100

    Fetching menu of a given date. Added "jsconfig.json" for Visual Studio Code

--- helpers/getReply.js
+++ helpers/getReply.js
@@ -2,28 +2,36 @@ var request = require('request'),
 	cheerio = require('cheerio'),
 	iconv = require('iconv-lite');
 
-var getFood = function(callback) {
+var getFood = function(date, callback) {
 	request.get({
 		url: 'https://sigarra.up.pt/sasup/pt/web_base.gera_pagina?P_pagina=265689',
 		encoding: null
 	}, function(error, response, body) {
 		if (error || response.statusCode != 200) {
-			callback('Erro ao procurar a ementa.');
+			return callback('Erro ao procurar a ementa.');
 		}
 		else {
 			var $ = cheerio.load(iconv.decode(new Buffer(body), 'iso-8859-1'));
 			
-			var food = $('#cn-eng').children('p').first().text();
+			var currentElement = $('#cn-eng').children().first();
 			
-			console.log(food);
-			
-			callback(food);
+			if (currentElement.next()) {
+				currentElement = currentElement.next();
+				
+				do {
+					if (currentElement.children('.date').text().trim() == date) {
+						return callback(currentElement.next().text().trim());
+					}
+				} while (currentElement.next().next())
+			}
 		}
+		
+		callback('Desculpa, mas não consegui encontrar informações para esse dia!');
 	});
 }
 
 module.exports = function(message, reply) {
-	getFood(function(food) {
+	getFood(message, function(food) {
 		reply(food);
 	});
 }
